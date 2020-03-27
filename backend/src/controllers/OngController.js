2802c1e10ca38a81c1944d58d073e4c01c4c94df
Thome <thomeneto@hotmail.com>
2020-03-26 21:52:50 -0300

    Front-end do app

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,24 +2,24 @@ const connection = require('../database/connection');
 const crypto = require('crypto');
 
 module.exports = {
-    async index(request, response) {
-        const ongs = await connection('ongs').select('*');
-    
-        return response.json(ongs);
-    },
+  async index(request, response) {
+    const ongs = await connection('ongs').select('*');
 
-    async create(request, response) {
-        const { name, email, whatsapp, city, uf } = request.body;
-        const id = crypto.randomBytes(4).toString('HEX');
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf,
-        });
+    return response.json(ongs);
+  },
 
-        return response.json({ id });
-    }
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    });
+
+    return response.json({ id });
+  }
 }
